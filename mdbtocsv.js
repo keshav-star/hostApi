7c83e7e847cf5c74853cf4b32e1c2529e3bd4857
keshav-star <keshavsandhu00@gmail.com>
2023-08-31 14:05:08 +0530

    hosting

---
+++ mdbtocsv.js
@@ -0,0 +1,42 @@
+import {query } from "@el3um4s/node-mdb";
+import fs from 'fs'
+const database = "./Weighing_API.mdb";
+
+// const result = await table.list({ database });
+const sql = `
+  SELECT * from API_DATA;`;
+const result = await query.sql({
+  database,
+  sql,
+})
+
+
+// Filter out the first and second properties
+const filteredData = result.map(item => {
+    const { T_ID, WB_Location_ID, ...rest } = item;
+    return rest;
+});
+
+// Create a JSON object with keys and values in inverted commas
+const formattedJSON = {};
+
+for (let i = 0; i < filteredData.length; i++) {
+    const formattedItem = {};
+    for (const [key, value] of Object.entries(filteredData[i])) {
+        formattedItem[`${key}`] = `${value}`;
+    }
+    formattedJSON[result[i].T_ID] = formattedItem;
+    console.log(result[i].WB_Location_ID)
+  }
+  let newResult = {};
+  newResult[result[0].WB_Location_ID] = formattedJSON;
+
+console.log(newResult);
+// console.log(result);
+fs.writeFile('db.json', JSON.stringify(newResult, null, 2), (err) => {
+  if (err) {
+    console.error('Error writing to db.json:', err);
+    return;
+  }
+  console.log('Data written to db.json');
+});
